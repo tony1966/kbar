0f764b123d36bd16c206e65b9b6edf5a20bd0c2b
Tony Huang <tony1966@ms5.hinet.net>
2025-11-22 23:52:22 +0800

    Add files via upload

--- src/kbar/__init__.py
+++ src/kbar/__init__.py
@@ -1,3 +1,8 @@
+"""
+KBar: A lightweight stock candlestick plotting utility with Chinese font auto-detection.
+"""
+
 from .kbar import KBar, detect_font
 
 __all__ = ["KBar", "detect_font"]
+__version__ = "0.1.3"
